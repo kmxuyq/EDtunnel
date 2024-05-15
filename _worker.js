2939db788d11407aabba1df81c4bad03677d75a0
kmxuyq <kmxuyq@163.com>
2024-05-15 11:39:24 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -5,7 +5,7 @@ import { connect } from 'cloudflare:sockets';
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
 let userID = 'ef70487f-2f32-4fe8-8aef-7b53fbf4babe';
 
-const proxyIPs = ['cdn-all.xn--b6gac.eu.org','cdn.xn--b6gac.eu.org','cdn-b100.xn--b6gac.eu.org','edgetunnel.anycast.eu.org','cdn.anycast.eu.org','workers.cloudflare.cyou'];
+const proxyIPs = ['workers.cloudflare.cyou'];
 
 // if you want to use ipv6 or single proxyIP, please add comment at this line and remove comment at the next line
 let proxyIP = proxyIPs[Math.floor(Math.random() * proxyIPs.length)];
